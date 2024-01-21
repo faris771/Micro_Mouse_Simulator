79e8599d4f2b266cc8dd3c261526c0078d8bdb60
Omar <112338255+Omarmasalmah@users.noreply.github.com>
2024-01-21 16:33:48 +0200

    all works

--- Algorithms/HandRule.py
+++ Algorithms/HandRule.py
@@ -1,23 +1,19 @@
 import time
-
 from constants import *
 from Algorithms.Algorithm import Algorithm
-
 import API
-
 
 class HandRule(Algorithm):
 
     def execute(self):
         pass
 
-
 class LeftHandRule(HandRule):
     def check_end_point(self, current_position):
-        if (current_position[0] == CENTER and current_position[0] == CENTER) or (
-                current_position[0] == CENTER - 1 and current_position[0] == CENTER - 1) or (
-                current_position[0] == CENTER - 1 and current_position[0] == CENTER) or (
-                current_position[0] == CENTER and current_position[0] == CENTER - 1):
+        if (current_position[0] == CENTER and current_position[1] == CENTER) or (
+                current_position[0] == CENTER - 1 and current_position[1] == CENTER - 1) or (
+                current_position[0] == CENTER - 1 and current_position[1] == CENTER) or (
+                current_position[0] == CENTER and current_position[1] == CENTER - 1):
             return True
 
     def execute(self):
@@ -26,34 +22,40 @@ class LeftHandRule(HandRule):
 
         current_position = [0, 0]
         row, col = current_position
+        oriantation = 0
 
         while True:
 
-            # if self.check_end_point(current_position):
-            #     break
-
-            # API.setColor(current_position[1], current_position[0], 'B')
 
             if not API.wallLeft():
                 API.turnLeft()
-                current_position[0] -= 1
+                oriantation = (oriantation-1) %4
+
             while API.wallFront():
                 API.turnRight()
-                current_position[0] += 1
-                # API.setColor(current_position[1], current_position[0], 'G')
+                oriantation = (oriantation+1) %4
+
 
             API.moveForward()
-            current_position[0] += 1
 
-            # API.setColor(current_position[1], current_position[0], 'G')
+            if oriantation ==0:
+                current_position[1] += 1
+            elif oriantation ==1:
+                current_position[0] += 1
+            elif oriantation ==2:
+                current_position[1] -= 1
+            elif oriantation ==3:
+                current_position[0] -= 1
+            API.setColor(current_position[0], current_position[1], 'G')
+            
 
 
 class RightHandRule(HandRule):
     def check_end_point(self, current_position):
-        if (current_position[0] == CENTER and current_position[0] == CENTER) or (
-                current_position[0] == CENTER - 1 and current_position[0] == CENTER - 1) or (
-                current_position[0] == CENTER - 1 and current_position[0] == CENTER) or (
-                current_position[0] == CENTER and current_position[0] == CENTER - 1):
+        if (current_position[0] == CENTER and current_position[1] == CENTER) or (
+                current_position[0] == CENTER - 1 and current_position[1] == CENTER - 1) or (
+                current_position[0] == CENTER - 1 and current_position[1] == CENTER) or (
+                current_position[0] == CENTER and current_position[1] == CENTER - 1):
             return True
 
     def execute(self):
@@ -63,6 +65,7 @@ class RightHandRule(HandRule):
 
         current_position = [0, 0]
         row, col = current_position
+        oriantation = 0
 
         while True:
 
@@ -71,14 +74,26 @@ class RightHandRule(HandRule):
 
             if not API.wallRight():
                 API.turnRight()
-                current_position[0] += 1
-                API.setColor(current_position[1], current_position[0], 'G')
+                oriantation = (oriantation+1) %4
+                #current_position[0] += 1
+                #API.setColor(current_position[1], current_position[0], 'G')
 
             while API.wallFront():
                 API.turnLeft()
-                current_position[0] -= 1
-                API.setColor(current_position[1], current_position[0], 'G')
+                oriantation = (oriantation-1) %4
+                #current_position[0] -= 1
+                #API.setColor(current_position[1], current_position[0], 'G')
 
             API.moveForward()
-            current_position[1] += 1
-            API.setColor(current_position[1], current_position[0], 'G')
+            #current_position[0] += 1
+            if oriantation ==0:
+                current_position[1] += 1
+            elif oriantation ==1:
+                current_position[0] += 1
+            elif oriantation ==2:
+                current_position[1] -= 1
+            elif oriantation ==3:
+                current_position[0] -= 1
+            
+            API.setColor(current_position[0], current_position[1], 'G')
+           
